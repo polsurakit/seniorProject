faaffc156c87dbf1d36c9b0c3e5e36260aa12ffd
Pol Surakitkoson <pol.surakit@gmail.com>
2019-02-19 03:08:51 +0700

    edit ui

--- simulator/main.cpp
+++ simulator/main.cpp
@@ -21,106 +21,87 @@ struct color{
 };
 
 //var
+const int TARGET_H_SIZE = 10000; //0.1 millimeter / pixel
+const int TARGET_W_SIZE = 10000; //0.1 millimeter / pixel
 myRandom randomGenerator;
-Mat field(10000,10000,CV_8UC3,Vec3b(0,0,0)); //0.1 millimeter / pixel
+const int fieldSize = 15000; //1.5m x 1.5m field
+Mat field(fieldSize,fieldSize,CV_8UC3,Vec3b(0,0,0)); //0.1 millimeter / pixel
 Mat image;
 Mat image2;
 Mat showedImage;
 Mat expectedImage;
-int TOPLEFTX = 100;
-int TOPLEFTY = 100;
-
-
+int TOPLEFTX = 1000;
+int TOPLEFTY = 1000;
+const int smallWindowSize = 250;
+const int bigWindowSize = 500;
 
 myPrinter printer(0,0,0,field);
 
-
-
 void showResult(){
-    printer.getCameraImage();
-    Mat res;
-    Mat res2;
-    Mat res3;
-    Mat res4;
-    vector<Vec3b> v(250000);
-    for(int i = -200 ; i < 300 ; i++){
-        for(int j = -200 ; j < 300 ; j++){
+    Mat window;
+    Mat window2;
+    int bigRatio = fieldSize/bigWindowSize;
+    resize(printer.cameraImage, window, cv::Size(smallWindowSize, smallWindowSize), 0, 0);
+    resize(field, window2, cv::Size(bigWindowSize, bigWindowSize), 0, 0);
+
+    //plot printer
+    for(int i = -200 ; i < 200 ; i++){
+        for(int j = -200 ; j < 200 ; j++){
             if(i*i+j*j > 100*100) continue;
-            v[(i+200)*500+j+200] = field.at<Vec3b>(printer.y+i,printer.x+j);
-            field.at<Vec3b>(printer.y+i,printer.x+j) = Vec3b(255,0,255);
+            window2.at<Vec3b>((printer.y+i)/bigRatio, (printer.x+j)/bigRatio) = Vec3b(255,0,255);
         }
     }
-    vector<Vec3b> v2;
-    for(int i = -printer.camera.rowSize/2 ; i < printer.camera.rowSize/2 ; i++){
-        int j = printer.camera.colSize/2;
-        int posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
-        int posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
-        posX = posX + printer.x;
-        posY = posY + printer.y;
-        if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
-            
-        }else{
-            //cout << posY << " " << posX << endl;
-            v2.push_back(field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2));
-            field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2) = Vec3b(0,0,255);
-        }
-        j = printer.camera.colSize/2-1;
-        posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
-        posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
-        posX = posX + printer.x;
-        posY = posY + printer.y;
-        if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
-            
-        }else{
-            //cout << posY << " " << posX << endl;
-            v2.push_back(field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2));
-            field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2) = Vec3b(0,0,255);
-        }
-        
+    //plot line =
+	for(int i = -printer.camera.rowSize/2 ; i < printer.camera.rowSize/2 ; i++){
+       int j = -printer.camera.colSize/2;
+       int posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
+       int posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
+       posX = posX + printer.x;
+       posY = posY + printer.y;
+       if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
+
+       }else{
+           window2.at<Vec3b>(posY/bigRatio, posX/bigRatio) = Vec3b(0,0,255);
+       }
+       j = printer.camera.colSize/2-1;
+       posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
+       posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
+       posX = posX + printer.x;
+       posY = posY + printer.y;
+       if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
+
+       }else{
+           window2.at<Vec3b>(posY/bigRatio, posX/bigRatio) = Vec3b(0,0,255);
+       }
     }
-    
-    resize(printer.cameraImage, res, cv::Size(250,250), 0, 0);
-    resize(field, res2, cv::Size(500,500), 0, 0);
-    
-    vconcat(res,showedImage,res);
-    hconcat(res,res2,res);
-    hconcat(res,expectedImage,res);
-    imshow( "Display window", res);
-    for(int i = -200 ; i < 300 ; i++){
-        for(int j = -200 ; j < 300 ; j++){
-            field.at<Vec3b>(printer.y,printer.x) = v[(i+200)*500+j+200];
-        }
+    //plot line ||
+    for(int j = -printer.camera.colSize/2 ; j < printer.camera.colSize/2 ; j++){
+       int i = -printer.camera.rowSize/2;
+       int posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
+       int posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
+       posX = posX + printer.x;
+       posY = posY + printer.y;
+       if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
+
+       }else{
+           window2.at<Vec3b>(posY/bigRatio, posX/bigRatio) = Vec3b(0,0,255);
+       }
+       i = printer.camera.rowSize/2-1;
+       posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
+       posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
+       posX = posX + printer.x;
+       posY = posY + printer.y;
+       if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
+
+       }else{
+           window2.at<Vec3b>(posY/bigRatio, posX/bigRatio) = Vec3b(0,0,255);
+       }
     }
-    int idx = 0;
-    for(int i = -printer.camera.rowSize/2 ; i < printer.camera.rowSize/2 ; i++){
-        int j = printer.camera.colSize/2;
-        int posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
-        int posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
-        posX = posX + printer.x;
-        posY = posY + printer.y;
-        if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
-            
-        }else{
-            //cout << posY << " " << posX << endl;
-            //v2.push_back(field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2));
-            field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2) = v2[idx];
-            idx++;
-        }
-        j = printer.camera.colSize/2-1;
-        posX = round(cos(printer.theta)*(i+0.5) - sin(printer.theta)*(j+0.5));
-        posY = round(sin(printer.theta)*(i+0.5) + cos(printer.theta)*(j+0.5));
-        posX = posX + printer.x;
-        posY = posY + printer.y;
-        if(posX < 0 || posY < 0 || posX >= field.cols || posY >= field.rows){
-            
-        }else{
-            //cout << posY << " " << posX << endl;
-            //v2.push_back(field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2));
-            field.at<Vec3b>(j+printer.camera.rowSize/2,i+printer.camera.colSize/2) = v2[idx];
-            idx++;
-        }
-        
-    }
+
+    vconcat(window,showedImage,window);
+    hconcat(window,window2,window);
+    hconcat(window,expectedImage,window);
+    imshow( "Display window", window);
     waitKey(0);
 }
 
@@ -130,35 +111,28 @@ void initialize(){
     {
         cout <<  "Could not open or find the image" << std::endl ;
     }
-    resize(image,showedImage,Size(250,250),0,0);
-    
-    for(int i = TOPLEFTY ; i <= 10000-TOPLEFTY ; i++){
-        for(int j = TOPLEFTX ; j <= 10000-TOPLEFTX ; j++){
+    resize(image,showedImage,Size(smallWindowSize, smallWindowSize),0,0);
+    for(int i = TOPLEFTY ; i <= fieldSize-TOPLEFTY ; i++){
+        for(int j = TOPLEFTX ; j <= fieldSize-TOPLEFTX ; j++){
             field.at<Vec3b>(i,j) = Vec3b(255,255,255);
         }
     }
-    resize(image,image2,Size(7000,7000),0,0);
-    
-    for(int i = TOPLEFTY+1 ; i <= TOPLEFTY+7000 ; i++){
-        for(int j = TOPLEFTX+1 ; j <= TOPLEFTX+7000 ; j++){
+    resize(image,image2,Size(TARGET_H_SIZE, TARGET_W_SIZE),0,0);
+    for(int i = TOPLEFTY+1 ; i <= TOPLEFTY+TARGET_H_SIZE ; i++){
+        for(int j = TOPLEFTX+1 ; j <= TOPLEFTX+TARGET_W_SIZE ; j++){
             field.at<Vec3b>(i,j) = image2.at<Vec3b>(i-TOPLEFTY-1,j-TOPLEFTX-1);
-            //cout << field.at<Vec3b>(i,j) << " ";
         }
     }
-    
-    resize(field,expectedImage,Size(500,500),0,0);
-    for(int i = TOPLEFTY+1 ; i <= TOPLEFTY+7000 ; i++){
-        for(int j = TOPLEFTX+1 ; j <= TOPLEFTX+7000 ; j++){
+    resize(field,expectedImage,Size(bigWindowSize,bigWindowSize),0,0);
+    for(int i = TOPLEFTY+1 ; i <= TOPLEFTY+TARGET_H_SIZE ; i++){
+        for(int j = TOPLEFTX+1 ; j <= TOPLEFTX+TARGET_W_SIZE ; j++){
             field.at<Vec3b>(i,j) = Vec3b(255,255,255);
-            //cout << field.at<Vec3b>(i,j) << " ";
         }
     }
-
 }
 
 void algorithm(){
     showResult();
-    
 }
 
 
@@ -166,6 +140,7 @@ void algorithm(){
 int main(int argc, char** argv)
 {
     initialize();
+    printer.getCameraImage();
     algorithm();
     return 0;
 }
